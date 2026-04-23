059170c5a4ae350305c1792ced78785d0ab3fac4
Hector <hector.pasantes.7e9@itb.cat>
2026-04-23 16:28:25 +0200

    Added Rogue Creation Page. Closes #5

--- Pages/Heroes/CreateRogue.cshtml.cs
+++ Pages/Heroes/CreateRogue.cshtml.cs
@@ -1,3 +1,5 @@
+using HeroEngine.Core.Models;
+using HeroEngine.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,22 @@ namespace HeroEngine.Pages.Heroes
 {
     public class CreateRogueModel : PageModel
     {
-        public void OnGet()
+        private readonly PartyService _partyService;
+
+        public CreateRogueModel(PartyService partyService)
         {
+            _partyService = partyService;
+        }
+        [BindProperty]
+        public string RogueName { get; set; }
+
+        public IActionResult OnPostAdd()
+        {
+            var newRogue = new Rogue(RogueName, Rogue.RogueBaseHp, 1, 1, 5);
+
+            _partyService.Add(newRogue);
+
+            return RedirectToPage("Heroes");
         }
     }
 }
--- Services/PartyService.cs
+++ Services/PartyService.cs
@@ -7,14 +7,18 @@ namespace HeroEngine.Services
         private readonly List<AHero> _party = new()
         {
             new Warrior ("Pepe", Warrior.WarriorBaseHp, 1, 1, 45, "a"),
-            new Mage ("Pepe", Mage.BaseMageHp, 1, 1, 45, 1),
-            new Rogue ("Pepe", Rogue.RogueBaseHp, 1, 1, 45)
+            new Mage ("Pepe", Mage.BaseMageHp, 1, 2, 45, 1),
+            new Rogue ("Pepe", Rogue.RogueBaseHp, 1, 3, 45)
         };
-        private int _nextId = 0;
+        private int _nextId = 4;
 
         public List<AHero> GetAll() => _party;
         public AHero? GetById(int id) => _party.FirstOrDefault(g => g.Id == id);
-        public void Add(AHero game) { game.Id = _nextId++; _party.Add(game); }
+        public void Add(AHero hero)
+        {
+            hero.Id = _nextId++;
+            _party.Add(hero);
+        }
         public void Update(AHero game)
         {
             var index = _party.FindIndex(g => g.Id == game.Id);
