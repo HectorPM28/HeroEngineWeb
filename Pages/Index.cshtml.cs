2174d734b3507ed48130273842885b1827a04d40
Hector <hector.pasantes.7e9@itb.cat>
2026-04-26 22:30:46 +0200

    added index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,10 +7,25 @@ namespace HeroEngine.Pages
 {
     public class IndexModel : PageModel
     {
-        
+
+        private readonly PartyService _partyService;
+
+        public IndexModel(PartyService partyService)
+        {
+            _partyService = partyService;
+        }
+
+        public int HeroCount { get; set; }
+        public List<AHero> LastHeroes { get; set; }
+
         public void OnGet()
         {
+            var allHeroes = _partyService.GetAll();
 
+            HeroCount = allHeroes.Count;
+
+            // 2. Tomamos solo los últimos 3 o 4 para el resumen
+            LastHeroes = allHeroes.TakeLast(4).Reverse().ToList();
         }
     }
 }
